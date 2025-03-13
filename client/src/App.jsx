b15a1419ea2767c3caf7359bfce0ff932c8fbc10
Aakash Jain <118706951+aakasshhh@users.noreply.github.com>
2025-03-13 19:22:08 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react'
 import {Routes,Route} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import Home from './pages/Home'
 import Result from './pages/Result'
 import BuyCredit from './pages/BuyCredit'
@@ -9,6 +11,7 @@ import Login from './components/Login'
 import { AppContext } from './context/AppContext'
 
 
+
 const App = () => {
 
   const {showLogin} = useContext(AppContext)
@@ -16,6 +19,7 @@ const App = () => {
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 
     min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <ToastContainer position='bottom-right'/>
 
       <NavBar/>
       {showLogin && <Login/>}
